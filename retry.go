b69013675b49dea51959c6c1f72ce7e05fbf44b4
任勇全 <ryq@meitu.com>
2018-03-06 19:16:10 +0800

    First commit

---
+++ retry.go
@@ -0,0 +1,86 @@
+package retry
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrExceed = errors.New("exceed the max retry limit")
+)
+
+// Retriable is an error type which can be retried
+type Retriable error
+
+// Retry ensures that the do function will be executed until some condition being satisfied
+type Retry interface {
+	Ensure(ctx context.Context, do func() error) error
+}
+
+type retry struct {
+	backoff BackoffStrategy
+	base    time.Duration
+}
+
+// Ensure keeps retring until ctx is done
+func (r *retry) Ensure(ctx context.Context, do func() error) error {
+	duration := r.base
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		if err := do(); err != nil {
+			if _, ok := err.(Retriable); ok {
+				if r.backoff != nil {
+					duration = r.backoff(duration)
+
+					time.Sleep(duration)
+				}
+				continue
+			}
+			return err
+		}
+
+		return nil
+	}
+}
+
+// Option is an option to new a Retry object
+type Option func(r *retry)
+
+// BackoffStrategy defines the backoff strategy of retry
+type BackoffStrategy func(last time.Duration) time.Duration
+
+// WithBackoff replace the default backoff function
+func WithBackoff(backoff BackoffStrategy) Option {
+	return func(r *retry) {
+		r.backoff = backoff
+	}
+}
+
+// WithBase set the first delay duration, default 10ms
+func WithBaseDelay(base time.Duration) Option {
+	return func(r *retry) {
+		r.base = base
+	}
+}
+
+// New a retry object
+func New(opts ...Option) Retry {
+	r := &retry{base: 10 * time.Millisecond, backoff: Exponential(2)}
+	for _, opt := range opts {
+		opt(r)
+	}
+	return r
+}
+
+// Exponential generates backoff duration by expoential
+func Exponential(factor float64) BackoffStrategy {
+	return func(last time.Duration) time.Duration {
+		return time.Duration(float64(last) * factor)
+	}
+}
---
+++ retry_test.go
@@ -0,0 +1,75 @@
+package retry
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestExponential(t *testing.T) {
+	exp := Exponential(2)
+	if d := exp(0); d != 0 {
+		t.Fatal(d)
+	}
+
+	if d := exp(1); d != 2 {
+		t.Fatal(d)
+	}
+
+	if d := exp(10); d != 20 {
+		t.Fatal(d)
+	}
+
+	exp = Exponential(1.8)
+	if d := exp(10); d != 18 {
+		t.Fatal(d)
+	}
+}
+
+func TestEnsure(t *testing.T) {
+	r := New(WithBaseDelay(1 * time.Millisecond))
+
+	val := 0
+	do := func() error {
+		val++
+		t.Log(val)
+		if val == 5 {
+			return nil
+		}
+		return Retriable(errors.New("please retry"))
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	if err := r.Ensure(ctx, do); err != nil {
+		t.Fatal(err)
+	}
+
+	if val != 5 {
+		t.Fatal(val)
+	}
+}
+
+func TestWithBaseDelay(t *testing.T) {
+	r := &retry{}
+	opt := WithBaseDelay(1)
+	opt(r)
+
+	if r.base != 1 {
+		t.Fatal(r.base)
+	}
+}
+func TestWithBackoff(t *testing.T) {
+	r := &retry{}
+	opt := WithBackoff(nil)
+	opt(r)
+	if r.backoff != nil {
+		t.Fatal(r.backoff)
+	}
+
+	opt = WithBackoff(Exponential(2))
+	opt(r)
+	if r.backoff == nil {
+		t.Fatal(r.backoff)
+	}
+}
