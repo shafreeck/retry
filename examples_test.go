649f57105bbe39d31a941f633f94cb748db9e102
任勇全 <ryq@meitu.com>
2018-07-19 11:53:14 +0000

    Remove unused example code

--- examples_test.go
+++ examples_test.go
@@ -8,45 +8,10 @@ import (
 	"time"
 )
 
-func ExampleHTTPGet() {
-	var resp *http.Response
-	var err error
-	get := func() error {
-		resp, err = http.Get("http://www.example.com")
-		if err != nil {
-			return Retriable(err)
-		}
-		return nil
-	}
-
+func ExampleEnsure() {
 	r := New()
-	ctx, _ := context.WithTimeout(context.Background(), time.Second)
-	if err := r.Ensure(ctx, get); err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(resp)
-}
-
-func ExampleHTTPGetEasier() {
-	r := New()
-	ctx, _ := context.WithTimeout(context.Background(), time.Second)
-	err := r.Ensure(ctx, func() error {
-		resp, err := http.Get("http://www.example.com")
-		if err != nil {
-			return Retriable(err)
-		}
-
-		log.Println(resp)
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func ExampleHTTPGetAndPost() {
-	r := New()
-	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
 	err := r.Ensure(ctx, func() error {
 		resp, err := http.Get("http://www.example.com")
